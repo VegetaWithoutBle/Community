05a8a67cd4c72da40f8148620f3c4b21128eda89
丁阳 <462945951@qq.com>
2020-04-04 05:59:51 +0800

    5

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -8,6 +8,7 @@ import life.majiang.community.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,7 @@ import java.util.UUID;
  */
 @Controller
 @Slf4j
+@PropertySource({"classpath:application.properties"})
 public class AuthorizeController {
 
     @Autowired
